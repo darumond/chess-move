3b2b394ff49fff61b53c979151529b490c72be67
luc.nguyen <luc.nguyen@epita.fr>
2023-08-21 03:20:50 +0300

    feat: switch to composition api

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,50 +2,30 @@
   <div class="main">
     <h1>Chess Move Calculator</h1>
     <div id="board1" style="width: 400px"></div>
-    <!-- <textarea cols="38" @change="getPosition">
-{{ fenUpdate }}
-</textarea> -->
-<input type="text" v-model="fenUpdate" @input="getPosition"/>
-    <button @click="getPosition">Get Position</button>
+    <input
+      @input=chessboard.position(fenUpdate);
+      type="text"
+      v-model="fenUpdate"
+    />
   </div>
 </template>
 
-<script>
+<script setup>
 import ChessBoard from "chessboardjs-vue3";
-import { RouterLink } from "vue-router";
-import { computed, reactive, ref, watch } from 'vue'
+import { ref, onMounted } from 'vue'
 
 const fenUpdate = ref("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR");
-
-function onChange (oldPos, newPos) {
-  fenUpdate.value = Chessboard.objToFen(newPos);
-}
 const config = {
   position: fenUpdate.value,
   draggable: true,
-  onChange: onChange,
+  onChange: (oldPos, newPos) => {
+    fenUpdate.value = Chessboard.objToFen(newPos);
+  }
 }
-let res;
-let test;
-export default {
-  data() {
-    return {
-      getPosition: getPosition,
-      fenUpdate: fenUpdate,
-      res: res,
-    };
-  },
-  mounted() {
-    res = ChessBoard('board1', config);
-  },
-  
-}
-const getPosition = () => {
-  console.log(res.fen());
-  console.log(fenUpdate.value);
-  res.position(fenUpdate.value);
-}
-
+let chessboard;
+onMounted(() => {
+  chessboard = ChessBoard('#board1', config);
+});
 </script>
 
 <style>
